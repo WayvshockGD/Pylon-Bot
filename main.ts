72c3b00ca4de7e85e9956d89e3e0e6f4f04b3612
Nimbi <Darkwater409@gmail.com>
2020-12-24 09:28:12 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,2 @@
-// config
-import './config/config';
-
-// events
-import './events/message';
-import './events/join-leave';
-import './events/roles';
-
-// commands
-import './commands/moderation/commands';
-import './commands/fun/commands';
-import './commands/economy/commands';
-import './commands/utility/commands';
+import './handler';
+import './globals';
